eb83cce9b1fcd876a3fa8dd5f4e683ca38f4392c
munnep <patrick.munne@hashicorp.com>
2022-12-18 15:40:50 +0100

    bridgecrew run task working

--- workspace_code/main.tf
+++ workspace_code/main.tf
@@ -1,1 +1,17 @@
-resource "null_resource" "test" {}
+terraform {
+  cloud {
+    organization = "patrickmunne"
+
+    workspaces {
+      name = "test_run_tasks"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "just_an_example"
+}
--- workspace_creation/main.tf
+++ workspace_creation/main.tf
@@ -16,11 +16,3 @@ resource "tfe_workspace" "test" {
   organization = "patrickmunne"
   tag_names    = ["test", "app"]
 }
-
-resource "tfe_organization_run_task" "example" {
-  organization =  "patrickmunne"
-  url          = "https://639f16f47aaf11ceb89234ae.mockapi.io/api/v1/test"
-  name         = "task-name"
-  enabled      = true
-  description  = "An example task"
-}
